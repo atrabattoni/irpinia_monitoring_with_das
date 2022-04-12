be790bb0d9ad60dbf2646ec805213ce653235c63
atrabttoni <alister.trabattoni@gmail.com>
2022-04-12 17:10:23 +0200

    Fix figure numbering.

---
+++ figs/fig8.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# noise statistics
+q10 = 6.980e-10
+q50 = 1.252e-09
+q90 = 4.842e-09
+
+# estimated power density
+dataset = pd.read_csv("../data/power_densities.csv")
+signal_kinetic = dataset["signal_kinetic"]
+signal_elastic = dataset["signal_elastic"]
+noise_kinetic = dataset["noise_kinetic"]
+noise_elastic = dataset["noise_elastic"]
+
+# signal to noise ratio
+snr = (signal_elastic - noise_elastic) / noise_elastic
+mask = snr > 2
+
+# fit line on detected events
+p = np.polyfit(np.log(signal_kinetic[mask]), np.log(
+    signal_elastic[mask]) - np.log(signal_kinetic[mask]), 0)
+x = np.array([1e-9, 1e-5])
+y = x * np.exp(p)
+
+# plot
+mm = 1 / 25.4
+plt.style.use("figure.mplstyle")
+plt.figure(dpi=300, figsize=(95 * mm, 80 * mm), constrained_layout=True)
+plt.loglog(x, y, "--", color="black", label="Linear fit")
+plt.axhline(q10, lw=0.75, c="black", ls=":", alpha=0.5, label="DAS noise 10th and 90th quantile")
+plt.axhline(q50, lw=0.75, c="black", ls="-", alpha=0.5, label="DAS noise median")
+plt.axhline(q90, lw=0.75, c="black", ls=":", alpha=0.5)
+plt.loglog(signal_kinetic[~mask], signal_elastic[~mask], "X", c="C3", label="DAS SNR < 2")
+plt.loglog(signal_kinetic[mask], signal_elastic[mask], "o", c="C2", label="DAS SNR > 2")
+plt.legend()
+plt.ylabel("Elastic power density (DAS) [$J m^{-3} s^{-1}$]")
+plt.xlabel("Kinetic power density (COL3) [$J m^{-3} s^{-1}$]")
+plt.savefig("fig9.jpg")
+plt.close()
--- figs/fig9.py
+++ figs/fig9.py
@@ -1,41 +1,93 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sp
+import xarray as xr
+from obspy import read
 
-# noise statistics
-q10 = 6.980e-10
-q50 = 1.252e-09
-q90 = 4.842e-09
+from utils import bandpass, get_optimal
 
-# estimated power density
-dataset = pd.read_csv("../data/power_densities.csv")
-signal_kinetic = dataset["signal_kinetic"]
-signal_elastic = dataset["signal_elastic"]
-noise_kinetic = dataset["noise_kinetic"]
-noise_elastic = dataset["noise_elastic"]
+# parameters
+event = "20210909.032630"
+freqmin = 1
+freqmax = 20
+offset_lim = (60, 100)
+cmin = 60
+cmax = 300
+cstep = 10
 
-# signal to noise ratio
-snr = (signal_elastic - noise_elastic) / noise_elastic
-mask = snr > 2
+# load COL3 data
+st = read(f"../data/{event}.COL3.C03.IX.CN.sac")
+tr = st[0]
+sensibility = tr.stats.sac["user2"] / tr.stats.sac["user3"]
+tr.data = tr.data * sensibility
+tr.detrend()
+tr.taper(max_percentage=0.05)
+tr.filter("bandpass", freqmin=freqmin, freqmax=freqmax)
 
-# fit line on detected events
-p = np.polyfit(np.log(signal_kinetic[mask]), np.log(
-    signal_elastic[mask]) - np.log(signal_kinetic[mask]), 0)
-x = np.array([1e-9, 1e-5])
-y = x * np.exp(p)
+# get picked phases
+starttime = np.datetime64(tr.stats.starttime + tr.stats.sac.a - 5)
+endtime = np.datetime64(tr.stats.starttime + tr.stats.sac.t0 + 10)
+ptime = np.datetime64(tr.stats.starttime + tr.stats.sac.a)
+stime = np.datetime64(tr.stats.starttime + tr.stats.sac.t0)
+
+# load DAS data
+xarr = xr.open_dataarray(f"../data/{event}.DAS.nc")
+xarr = xarr.sel(
+    time=slice(starttime, endtime),
+    offset=slice(*offset_lim),
+)
+dt = (xarr["time"][1] - xarr["time"][0]).values / np.timedelta64(1, "s")
+fs = 1 / dt
+xarr.data = bandpass(xarr, freqmin, freqmax, fs, axis=0)
+
+# beamform
+copt, stack = get_optimal(xarr, cmin, cmax, cstep)
+seismo_enveloppe = np.abs(sp.hilbert(tr.data))
+coherent_enveloppe = np.abs(sp.hilbert(stack))
+incoherent_enveloppe = np.abs(sp.hilbert(xarr, axis=0)).mean(axis=1)
+tdas = xarr["time"].values
+tseis = (1e6*tr.times("timestamp")).astype("datetime64[us]")
 
 # plot
-mm = 1 / 25.4
 plt.style.use("figure.mplstyle")
-plt.figure(dpi=300, figsize=(95 * mm, 80 * mm), constrained_layout=True)
-plt.loglog(x, y, "--", color="black", label="Linear fit")
-plt.axhline(q10, lw=0.75, c="black", ls=":", alpha=0.5, label="DAS noise 10th and 90th quantile")
-plt.axhline(q50, lw=0.75, c="black", ls="-", alpha=0.5, label="DAS noise median")
-plt.axhline(q90, lw=0.75, c="black", ls=":", alpha=0.5)
-plt.loglog(signal_kinetic[~mask], signal_elastic[~mask], "X", c="C3", label="DAS SNR < 2")
-plt.loglog(signal_kinetic[mask], signal_elastic[mask], "o", c="C2", label="DAS SNR > 2")
-plt.legend()
-plt.ylabel("Elastic power density (DAS) [$J m^{-3} s^{-1}$]")
-plt.xlabel("Kinetic power density (COL3) [$J m^{-3} s^{-1}$]")
-plt.savefig("fig9.jpg")
-plt.close()
+mm = 1 / 25.4
+fig, axes = plt.subplots(
+    figsize=(95 * mm, 95*mm),
+    ncols=4, sharey=True, constrained_layout=True, dpi=300)
+
+ax = axes[0]
+ax.set_title("COL3[Z]\n")
+ax.fill_betweenx(tseis, -seismo_enveloppe * 1e6,
+                 seismo_enveloppe * 1e6, color="dimgray")
+ax.set_xlabel("Velocity\n[um/s]")
+ax.set_ylabel("Time")
+ax.set_xlim(-1, 1)
+ax.tick_params(axis='y', labelrotation=90)
+
+ax = axes[1]
+ax.set_title("DAS\nincoherent")
+ax.fill_betweenx(tdas, -incoherent_enveloppe,
+                 incoherent_enveloppe, color="dimgray")
+ax.set_xlabel("Strain-rate\n[nm/m]")
+ax.set_xlim(-200, 200)
+ax = axes[2]
+ax.set_title("DAS\ncoherent")
+ax.fill_betweenx(tdas, -coherent_enveloppe,
+                 coherent_enveloppe, color="dimgray")
+ax.set_xlabel("Strain-rate\n[nm/m]")
+ax.set_xlim(-200, 200)
+ax = axes[3]
+xarr.plot(ax=ax, rasterized=True, vmin=-200, vmax=200, yincrease=False)
+ax.set_title("DAS\n")
+ax.set_ylim(endtime, starttime)
+ax.set_xlim(60, 90)
+ax.set_xticks([60, 100])
+ax.set_xlabel("Offset\n[m]")
+ax.set_ylabel("")
+
+for ax in axes:
+    ax.axhline(ptime, lw=1, c="C3")
+    ax.axhline(stime, lw=1, c="C3")
+
+fig.savefig("fig10.jpg")
+plt.close(fig)
